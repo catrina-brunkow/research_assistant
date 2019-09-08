504507d8cf5032affa13eb3f3938eba351663f0a
ahewett93 <alex.dh7@protonmail.com>
2019-09-07 17:53:51 -0700

    added boyce.yml

--- tool/__init__.py
+++ tool/__init__.py
@@ -4,6 +4,6 @@ from flask import Flask
 app = Flask(__name__)
 # Security key. import secrets, secrets.token_hex(16)
 app.config['SECRET_KEY'] = '0480350659cd7743a0164e35e2ba0abb'
-
+app.config['DEBUG'] = False
 
 from tool import routes
